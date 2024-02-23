81b115c264402a5461988327bed55394bbf1cb6d
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-02-23 20:26:44 +0100

    Added comments, setter, getter, hashcode and equals methods, as well as an empty test class

--- commons/src/main/java/commons/EventRepoStorage.java
+++ commons/src/main/java/commons/EventRepoStorage.java
@@ -2,19 +2,32 @@ package commons;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class EventRepoStorage implements EventRepository {
     private List<Event> events;
 
+    /**
+     * Creates a new Event list
+     */
     public EventRepoStorage() {
         this.events = new ArrayList<>();
     }
 
+    /***
+     * Gets all the events in a new list
+     * @return all events
+     */
     @Override
     public List<Event> getAllEvents() {
         return new ArrayList<>(events);
     }
 
+    /***
+     * Gets the event that corresponds to the inviteCode
+     * @param inviteCode invite code of the desired event
+     * @return the event corresponding the invite code or null if it is not found
+     */
     @Override
     public Event getEventByInviteCode(String inviteCode) {
         return events.stream()
@@ -23,13 +36,70 @@ public class EventRepoStorage implements EventRepository {
                 .orElse(null);
     }
 
+    /***
+     * Creates a new event
+     * @param event the event to be added
+     */
     @Override
     public void createEvent(Event event) {
         events.add(event);
     }
 
+    /***
+     * Updates a specific event
+     * @param updatedEvent the event to be replaced/updated
+     */
     @Override
     public void updateEvent(Event updatedEvent) {
         // Implement logic to update the event in the repository
+    }
+
+    /***
+     * Getter for the events
+     * @return the events in a list
+     */
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    /***
+     * Setter for the events
+     * @param events the new events list
+     */
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    /***
+     * Basic equals method
+     * @param o the object to be compared with
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventRepoStorage that = (EventRepoStorage) o;
+        return Objects.equals(events, that.events);
+    }
+
+    /***
+     * Hash code generator for the events
+     * @return hashcode of the events
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(events);
+    }
+
+    /***
+     * Gives a human-friendly representation of the events list
+     * @return the events list in a human-friendly String
+     */
+    @Override
+    public String toString() {
+        return "EventRepoStorage{" +
+                "events=" + events +
+                '}';
     }
 }
---
+++ commons/src/test/java/commons/EventRepoStorageTest.java
@@ -0,0 +1,49 @@
+package commons;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventRepoStorageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllEvents() {
+    }
+
+    @Test
+    void getEventByInviteCode() {
+    }
+
+    @Test
+    void createEvent() {
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void getEvents() {
+    }
+
+    @Test
+    void setEvents() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
