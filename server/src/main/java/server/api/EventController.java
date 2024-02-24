cdab9b91fef9abae21038f70b163810ec7dc1146
Ipshit Raychaudhuri <I.Raychaudhuri@student.tudelft.nl>
2024-02-24 14:48:20 +0100

    Added javadoc comments for clarity

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -22,16 +22,30 @@ public class EventController {
     private final Random random;
     private final EventRepository repo;
 
+    /**
+     * The constructor for the EventController class
+     * @param random - The random used to get a random entry
+     * @param repo - The Event repository
+     */
     public EventController(Random random, EventRepository repo) {
         this.random = random;
         this.repo = repo;
     }
 
+    /**
+     * Returns all the Events currently stored
+     * @return - All the Events
+     */
     @GetMapping(path = { "", "/" })
     public List<Event> getAll() {
         return repo.findAll();
     }
 
+    /**
+     * Return a specific Event from its id
+     * @param id - The id of the event
+     * @return - The Event with the id specified
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Event> getById(@PathVariable("id") long id) {
         if (id < 0 || !repo.existsById(id)) {
@@ -40,6 +54,11 @@ public class EventController {
         return ResponseEntity.ok(repo.findById(id).get());
     }
 
+    /**
+     * Adds an Event to the repository
+     * @param event - The Event to be added
+     * @return - The saved Event
+     */
     @PostMapping(path = { "", "/" })
     public ResponseEntity<Event> add(@RequestBody Event event) {
 
@@ -52,10 +71,24 @@ public class EventController {
         return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Checks if the provided string is null or empty
+     * @param s - The string to be checked
+     * @return - True iff the string is neither null nor empty. False otherwise.
+     */
     private static boolean isNullOrEmpty(String s) { return s == null || s.isEmpty(); }
 
+    /**
+     * Checks if the provided list of Persons is null or empty
+     * @param e - The list to be checked
+     * @return - True iff the list is neither null nor empty. False otherwise.
+     */
     private static boolean isNullOrEmpty(List<Person> e) { return e == null || e.isEmpty(); }
 
+    /**
+     * Returns a random Event from the repository
+     * @return - A randomly selected Event
+     */
     @GetMapping("rnd")
     public ResponseEntity<Event> getRandom() {
         var events = repo.findAll();
