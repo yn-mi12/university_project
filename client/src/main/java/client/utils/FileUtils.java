fd0519d127dbca8f8beaef01ff4d5906cdefc1a4
Jake Nijssen <j.t.m.nijssen@student.tudelft.nl>
2024-02-25 17:13:52 +0100

    Add file utils for the config and language file system
    Signed-off-by: Jake Nijssen <j.t.m.nijssen@student.tudelft.nl>

---
+++ client/src/main/java/client/utils/FileUtils.java
@@ -0,0 +1,38 @@
+package client.utils;
+
+import client.Main;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+
+/**
+ * Collection of File utility methods.
+ *
+ * @author Jake Nijssen
+ */
+public final class FileUtils {
+
+    /**
+     * Copy's a file from the resource directory to a target {@link File} if the corresponding
+     * file does not exist yet.
+     *
+     * @param target The target file
+     * @param resource The name of the resource inside the resources' directory.
+     */
+    public static void copyFileIfNotExists(@NotNull File target, @NotNull String resource) {
+        if (!target.exists()) {
+            try (InputStream is = Objects.requireNonNull(Main.class.getResource(resource)).openStream()) {
+                Files.createFile(target.toPath());
+                Files.write(target.toPath(), is.readAllBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
