24bbfd735977be24c6250ceac4e07949b97041b4
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-01 21:13:52 +0100

    Deleted unused import so that pipeline works

--- server/src/main/java/server/database/ParticipantRepository.java
+++ server/src/main/java/server/database/ParticipantRepository.java
@@ -1,7 +1,6 @@
 package server.database;
 
 import commons.Participant;
-import commons.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
