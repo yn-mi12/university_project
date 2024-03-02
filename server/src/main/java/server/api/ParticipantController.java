4215a2c383520dff32bcbdc29fc4c1b90f979508
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-02 17:56:21 +0100

    Added comments and refactored getEmail

--- server/src/main/java/server/api/ParticipantController.java
+++ server/src/main/java/server/api/ParticipantController.java
@@ -21,16 +21,31 @@ public class ParticipantController {
     private final Random random;
     private final ParticipantRepository repo;
 
+    /**
+     * The constructor for the ParticipantController class
+     *
+     * @param random - The random used to get a random entry
+     * @param repo   - The Participant repository
+     */
     public ParticipantController(Random random, ParticipantRepository repo) {
         this.random = random;
         this.repo = repo;
     }
 
+    /**
+     * @return - all the participants currently stored
+     */
     @GetMapping(path = { "", "/" })
     public List<Participant> getAll() {
         return repo.findAll();
     }
 
+    /**
+     * Return a specific Participant from its id
+     *
+     * @param id - The id of the participant
+     * @return - The Participant with the id specified
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Participant> getById(@PathVariable("id") long id) {
         if (id < 0 || !repo.existsById(id)) {
@@ -39,6 +54,12 @@ public class ParticipantController {
         return ResponseEntity.ok(repo.findById(id).get());
     }
 
+    /**
+     * Adds a Participant to the repository
+     *
+     * @param participant - The Participant to be added
+     * @return - The saved Participant
+     */
     @PostMapping(path = { "", "/" })
     public ResponseEntity<Participant> save(@RequestBody Participant participant) {
 
@@ -50,8 +71,18 @@ public class ParticipantController {
         return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Checks if the provided string is null or empty
+     *
+     * @param s - The string to be checked
+     * @return - True iff the string is neither null nor empty. False otherwise.
+     */
     private static boolean isNullOrEmpty(String s) { return s == null || s.isEmpty(); }
 
+    /**
+     * Return a random Participant from the repo
+     * @return - a random Participant from the repository
+     */
     @GetMapping("rnd")
     public ResponseEntity<Participant> getRandom() {
         var events = repo.findAll();
@@ -59,12 +90,14 @@ public class ParticipantController {
         return ResponseEntity.ok(events.get(idx));
     }
 
+    /***
+     * Returns the email of a Participant by id, if it exists
+     * @param id - The id of the Participant
+     * @return - The Participants email
+     */
     @GetMapping(path = { "/email/{id}" })
     public ResponseEntity<Object> getEmail(@PathVariable("id") long id) {
-        if (id < 0 || !repo.existsById(id)) {
-            return ResponseEntity.badRequest().build();
-        }
-        if(isNullOrEmpty(repo.findById(id).get().getEmail())){
+        if (id < 0 || !repo.existsById(id) || isNullOrEmpty(repo.findById(id).get().getEmail())) {
             return ResponseEntity.badRequest().build();
         }
         else return ResponseEntity.ok(repo.findById(id).get().getEmail());
