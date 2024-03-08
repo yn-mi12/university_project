682b2975570e36ef7ccdb9e298a501617cc02451
Ipshit Raychaudhuri <I.Raychaudhuri@student.tudelft.nl>
2024-03-08 01:39:22 +0100

    Added a system to retrieve a user's past events

--- client/src/main/java/client/scenes/StartScreenCtrl.java
+++ client/src/main/java/client/scenes/StartScreenCtrl.java
@@ -4,18 +4,30 @@ import client.Config;
 import client.Main;
 import client.utils.ServerUtilsEvent;
 import com.google.inject.Inject;
+import commons.Event;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class StartScreenCtrl implements Initializable {
 
     private final ServerUtilsEvent server;
     private final EventCtrl evntCtrl;
+    private ObservableList<String> titles;
+    @FXML
+    private ListView<String> eventList;
     @FXML
     private ChoiceBox<String> languageBox;
     @FXML
@@ -41,6 +53,14 @@ public class StartScreenCtrl implements Initializable {
                 Main.reloadUI();
             }
         }));
+
+        eventList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                String id = eventList.getSelectionModel().getSelectedItem().split(": ")[0];
+                viewPastEvent(Long.valueOf(id));
+            }
+        });
     }
 
     public void createEvent() {
@@ -52,11 +72,38 @@ public class StartScreenCtrl implements Initializable {
     public void viewEvent() {
         var inviteCode = this.codeField.getText();
         clearFields();
-        //Will be used to view a created event
+        //Will be used to view an event joined using the invite code field
+    }
+
+    public void viewPastEvent(Long id) {
+        //Will be used to view a selected event in the recent events section
     }
 
     public void refresh() {
-        //Will be used to fetch previously joined events
+        List<Long> ids = new ArrayList<>();
+        File eventIDs = new File("client/src/main/java/client/utils/events.txt");
+        if(eventIDs.exists()) {
+            try {
+                Scanner idScanner = new Scanner(eventIDs);
+                idScanner.useDelimiter("\r?\n");
+                while(idScanner.hasNext()) {
+                    ids.add(idScanner.nextLong());
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        List<Event> events = new ArrayList<>();
+        for(Long id : ids) {
+            events.add(server.getByID(id));
+        }
+        List<String> titles = new ArrayList<>();
+        for(Event e : events) {
+            titles.add(e.getId() + ": " + e.getTitle());
+        }
+
+        eventList.setItems(FXCollections.observableList(titles));
     }
 
     private void clearFields() {
