ae2ce5ca91dff708f61c1723d938b9c74584890f
Yana Mihaylova <Y.R.Mihaylova@student.tudelft.nl>
2024-03-12 20:23:43 +0100

    Changed the MenuButton and added a TextArea so participants in the event can be seen, will fix the language properties later

--- client/src/main/java/client/scenes/EventOverviewNewCtrl.java
+++ client/src/main/java/client/scenes/EventOverviewNewCtrl.java
@@ -5,13 +5,19 @@ import client.Main;
 import client.utils.ServerUtilsEvent;
 import com.google.inject.Inject;
 import commons.Event;
+import commons.Participant;
+import jakarta.ws.rs.WebApplicationException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Modality;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static jakarta.ws.rs.core.Response.ok;
@@ -19,13 +25,16 @@ import static jakarta.ws.rs.core.Response.ok;
 public class EventOverviewNewCtrl implements Initializable {
     private final ServerUtilsEvent server;
     private final SplittyCtrl controller;
-
+    private List<Participant> participants;
+    @FXML
+    private TextArea participantText = new TextArea();
+    @FXML
+    private SplitMenuButton part;
     @FXML
     public Label eventTitle;
     @FXML
     private ChoiceBox<String> languageBox;
     public Event event;
-
 
 
     @Inject
@@ -34,15 +43,37 @@ public class EventOverviewNewCtrl implements Initializable {
         this.controller = eventCtrl;
     }
 
-    public Event getSelectedEvent(){
+    public Event getSelectedEvent() {
         return event;
     }
-    public void setSelectedEvent(Event selectedEvent){
+
+    public void setSelectedEvent(Event selectedEvent) {
         this.event = selectedEvent;
+        this.participants = event.getParticipants();
+        ObservableList<MenuItem> names = FXCollections.observableArrayList();
+        StringBuilder namesString = new StringBuilder();
+        int i = 0;
+        for (Participant p : participants) {
+            names.add(new MenuItem(p.getFirstName()));
+            namesString.append(p.getFirstName());
+            if (i < participants.size() - 1)
+                namesString.append(", ");
+            i++;
+        }
+        part.getItems().setAll(names);
+        participantText.setEditable(false);
+        participantText.setText(namesString.toString());
+        for (MenuItem mi : part.getItems()) {
+            mi.setOnAction(e -> {
+                part.setText(mi.getText());
+            });
+        }
     }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         languageBox.getItems().addAll(Config.get().getSupportedLocales().stream().map(Config.SupportedLocale::getName)
                 .toList());
         languageBox.setValue(Config.get().getCurrentLocaleName());
