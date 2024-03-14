1379cb5da330818b42d69828021e2a0d4bafa530
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-14 17:43:07 +0100

    Added the basic methods(from TestQuoteRepository) in TestParticipantRepository

--- server/src/test/java/server/api/ParticipantControllerTest.java
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -1,12 +1,22 @@
 package server.api;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ParticipantControllerTest {
 
+    public int nextInt;
+    private MyRandom random;
+    private TestParticipantRepository repo;
+    private ParticipantController partc;
+
     @BeforeEach
     void setUp() {
+        random = new MyRandom();
+        repo = new TestParticipantRepository();
+        partc = new ParticipantController(random, repo);
     }
 
     @Test
@@ -27,5 +37,17 @@ public class ParticipantControllerTest {
 
     @Test
     void getEmail() {
+    }
+
+    @SuppressWarnings("serial")
+    public class MyRandom extends Random {
+
+        public boolean wasCalled = false;
+
+        @Override
+        public int nextInt(int bound) {
+            wasCalled = true;
+            return nextInt;
+        }
     }
 }
