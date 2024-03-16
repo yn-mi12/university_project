8a973bc0d85cfb4b54da1e7a0b8977475bbb3b77
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-16 15:18:25 +0100

    InvitationCtrl constructor

--- client/src/main/java/client/scenes/InvitationCtrl.java
+++ client/src/main/java/client/scenes/InvitationCtrl.java
@@ -1,2 +1,17 @@
-package client.scenes;public class InvitationCtrl {
+package client.scenes;
+
+import client.utils.ServerUtilsEvent;
+import jakarta.inject.Inject;
+
+public class InvitationCtrl {
+
+    private final ServerUtilsEvent server;
+    private final SplittyCtrl mainCtrl;
+
+    @Inject
+    public InvitationCtrl(ServerUtilsEvent server, SplittyCtrl mainCtrl)
+    {
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
 }
