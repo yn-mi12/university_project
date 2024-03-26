1d2ea73d61d6af51ca85a1f0cfbae8bde50eccf6
Kasparas Savickis <k.savickis@student.tudelft.nl>
2024-03-26 23:19:09 +0100

    typo

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -87,7 +87,7 @@ public class EventController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Event> deleteById(@PathVariable("id") long id){
         if (id < 0 || !repo.existsById(id)) {
-            ;return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().build();
         }
         Event x = repo.findById(id).orElse(null);
         repo.deleteById(id);
