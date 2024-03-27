69ac0890dbeaae3f7fb9a686e0b30e0dfa4d2d61
Kasparas Savickis <k.savickis@student.tudelft.nl>
2024-03-27 01:42:30 +0100

    Fixed checkstyle. Removed unused Imports Deleted unused ExpenseParticipantRepository.java NO CHANGES, BUT STABLE AND WORKING BEFORE MERGING MAIN

--- server/src/main/java/server/database/ExpenseParticipantRepository.java
+++
@@ -1,8 +0,0 @@
-package server.database;
-
-import commons.Expense;
-import commons.ExpenseParticipant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseParticipantRepository extends JpaRepository<ExpenseParticipant, Long> {
-}
