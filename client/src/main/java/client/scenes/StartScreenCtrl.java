e9f0ffc7a6a86b2568e2e023164fa484a9b26935
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-27 22:37:13 +0100

    Changed LanguageButton in EventOverview as well

--- client/src/main/java/client/scenes/StartScreenCtrl.java
+++ client/src/main/java/client/scenes/StartScreenCtrl.java
@@ -9,7 +9,6 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
