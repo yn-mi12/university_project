b43cc41b149125ddc2587d18190e0c55821ac33d
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-27 22:07:03 +0100

    Changed language button on startscreen and added flags

--- client/src/main/java/client/scenes/StartScreenCtrl.java
+++ client/src/main/java/client/scenes/StartScreenCtrl.java
@@ -8,18 +8,22 @@ import commons.Event;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
 
 public class StartScreenCtrl implements Initializable {
 
@@ -28,7 +32,7 @@ public class StartScreenCtrl implements Initializable {
     @FXML
     private ListView<String> eventList;
     @FXML
-    private ChoiceBox<String> languageBox;
+    private ComboBox<Label> languageBox;
     @FXML
     private TextField titleField;
     @FXML
@@ -43,12 +47,42 @@ public class StartScreenCtrl implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        languageBox.getItems().addAll(Config.get().getSupportedLocales().stream().map(Config.SupportedLocale::getName)
-                .toList());
-        languageBox.setValue(Config.get().getCurrentLocaleName());
+        ObservableList<Label> x = FXCollections.observableArrayList();
+        List<Config.SupportedLocale> languages = Config.get().getSupportedLocales().stream().toList();
+        for(var item : languages)
+        {
+            Image icon;
+            String iconPath = "client/images/" + item.getCode() + ".png";
+            icon = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(iconPath)));
+            ImageView iconImageView = new ImageView(icon);
+            iconImageView.setFitHeight(25);
+            iconImageView.setPreserveRatio(true);
+            x.add(new Label(item.getName(), iconImageView));
+        }
+        languageBox.setItems(x);
+        languageBox.setCellFactory(new Callback<>() {
+            @Override
+            public ListCell<Label> call(ListView<Label> param) {
+                return new ListCell<>() {
+                    @Override
+                    protected void updateItem(Label item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item == null || empty) {
+                        }
+                        else {
+                            item.setTextFill(Color.color(0, 0, 0));
+                            setGraphic(item);
+                        }
+                    }
+                };
+            }
+        });
+        String current = String.valueOf(Config.get().getCurrentLocaleName());
+        languageBox.setValue(languageBox.getItems().stream()
+                .filter(l -> String.valueOf(l.getText()).equals(current)).findFirst().orElse(null));
         languageBox.getSelectionModel().selectedItemProperty().addListener(((obs, oldVal, newVal) -> {
             if (newVal != null) {
-                Config.get().setCurrentLocale(newVal);
+                Config.get().setCurrentLocale(newVal.getText());
                 Config.get().save();
                 Main.reloadUI();
             }
