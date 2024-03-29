4aa72418a9ab4c3ec698020688ec4b2b523d096b
Daniel Dumitru <a.d.dumitru@student.tudelft.nl>
2024-03-29 21:20:15 +0100

    Language Button now functional in Admin Overview. Removed Delete All Button as well.

--- client/src/main/java/client/scenes/AdminOverviewCtrl.java
+++ client/src/main/java/client/scenes/AdminOverviewCtrl.java
@@ -1,5 +1,6 @@
 package client.scenes;
 
+import client.Config;
 import client.Main;
 import client.utils.ServerUtilsEvent;
 import com.google.inject.Inject;
@@ -8,17 +9,21 @@ import jakarta.ws.rs.WebApplicationException;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AdminOverviewCtrl implements Initializable {
@@ -27,6 +32,9 @@ public class AdminOverviewCtrl implements Initializable {
     private final SplittyCtrl controller;
     public ListView<String> eventList;
     public Button showButton;
+    public ComboBox<Label> languageBox;
+    public Button showButtonD;
+    public Button showButtonE;
     private Stage primaryStage;
 
     @Inject
@@ -48,9 +56,52 @@ public class AdminOverviewCtrl implements Initializable {
             public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
                 if (eventList.getSelectionModel().getSelectedItem() != null) {
                     showButton.setDisable(false);
+                    showButtonD.setDisable(false);
+                    showButtonE.setDisable(false);
                 }
             }
         });
+
+        ObservableList<Label> x = FXCollections.observableArrayList();
+        List<Config.SupportedLocale> languages = Config.get().getSupportedLocales().stream().toList();
+        for(var item : languages)
+        {
+            Image icon;
+            String iconPath = "client/images/" + item.getCode() + ".png";
+            icon = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(iconPath)));
+            ImageView iconImageView = new ImageView(icon);
+            iconImageView.setFitHeight(25);
+            iconImageView.setPreserveRatio(true);
+            x.add(new Label(item.getName(), iconImageView));
+        }
+        languageBox.setItems(x);
+        languageBox.setCellFactory(new Callback<>() {
+            @Override
+            public ListCell<Label> call(ListView<Label> param) {
+                return new ListCell<>() {
+                    @Override
+                    protected void updateItem(Label item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item == null || empty) {
+                        }
+                        else {
+                            item.setTextFill(Color.color(0, 0, 0));
+                            setGraphic(item);
+                        }
+                    }
+                };
+            }
+        });
+        String current = String.valueOf(Config.get().getCurrentLocaleName());
+        languageBox.setValue(languageBox.getItems().stream()
+                .filter(l -> String.valueOf(l.getText()).equals(current)).findFirst().orElse(null));
+        languageBox.getSelectionModel().selectedItemProperty().addListener(((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                Config.get().setCurrentLocale(newVal.getText());
+                Config.get().save();
+                Main.refreshAdminOverview();
+            }
+        }));
     }
 
     public void goBack() {
--- client/src/main/java/client/scenes/AdminPopupCtrl.java
+++ client/src/main/java/client/scenes/AdminPopupCtrl.java
@@ -1,9 +1,11 @@
 package client.scenes;
 
+import client.Main;
 import client.utils.ServerUtilsEvent;
 import com.google.inject.Inject;
 import jakarta.ws.rs.WebApplicationException;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 
@@ -12,6 +14,7 @@ public class AdminPopupCtrl {
     private final ServerUtilsEvent server;
     private final SplittyCtrl controller;
     public TextField insertedToken;
+    public Label incorrectToken;
 
     @Inject
     public AdminPopupCtrl(ServerUtilsEvent server, SplittyCtrl eventCtrl) {
@@ -27,9 +30,13 @@ public class AdminPopupCtrl {
             if(server.checkToken(insertedToken.getText())) {
                 controller.setAdmin(true);
                 controller.showAdminOverview();
+                Main.refreshAdminOverview();
+                insertedToken.clear();
+                incorrectToken.visibleProperty().setValue(false);
             }
-            else System.out.println("NO!");
-            insertedToken.clear();
+            else{
+                incorrectToken.visibleProperty().setValue(true);
+            }
         } catch (WebApplicationException e) {
 
             var alert = new Alert(Alert.AlertType.ERROR);
