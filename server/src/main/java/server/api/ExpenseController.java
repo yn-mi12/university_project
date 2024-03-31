f390d99911928aa33bb34caace2dc16d21c72cd2
Maria Zmpainou <m.zmpainou@student.tudelft.nl>
2024-03-31 19:35:23 +0200

    fixed checkstylea

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -3,7 +3,6 @@ package server.api;
 import java.util.List;
 
 import commons.Expense;
-import commons.Participant;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
