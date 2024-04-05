036bf08945652e1080e8ac62ab562750824924e1
Kasparas Savickis <k.savickis@student.tudelft.nl>
2024-04-06 01:31:31 +0200

    Clean up of adminController:  - removed unused endpoint which used deprecated functionality

--- server/src/main/java/server/api/AdminController.java
+++ server/src/main/java/server/api/AdminController.java
@@ -3,11 +3,9 @@ package server.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import server.security.RequiresAdmin;
 import server.service.AdminTokenService;
 
 @Controller
@@ -24,12 +22,6 @@ public class AdminController {
     @PostMapping("/verify/{token}")
     public ResponseEntity<Boolean> verifyToken(@PathVariable("token") String token) {
         return ResponseEntity.ok(tokenService.verifyToken(token));
-    }
-
-    @RequiresAdmin
-    @GetMapping("/")
-    public ResponseEntity<String> someProtectedRoute() {
-        return ResponseEntity.ok("You got some juicy secrets :O");
     }
 
 }
