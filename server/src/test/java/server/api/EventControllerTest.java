813702b1f4e71c930064d0e1f16b42a02c41ef95
Kasparas Savickis <k.savickis@student.tudelft.nl>
2024-04-07 22:23:28 +0200

    EventControllerTest done

--- server/src/test/java/server/api/EventControllerTest.java
+++ server/src/test/java/server/api/EventControllerTest.java
@@ -1,93 +1,145 @@
 package server.api;
 
 import commons.Event;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.*;
 
 class EventControllerTest {
 
     private TestEventRepository repo;
     private EventController eventc;
+    private Event x1;
+    private Event x2;
+    private Event x3;
 
     @BeforeEach
     void setUp() {
         repo = new TestEventRepository();
         eventc = new EventController(repo);
+        x1 = new Event("Event 1");
+        x2 = new Event("Event 2");
+        x3 = new Event("Event 3");
+        eventc.save(x1);
+        eventc.save(x2);
+        eventc.save(x3);
     }
 
     @Test
     void getAll() {
-        Event x1 = new Event("Event 1");
-        Event x2 = new Event("Event 2");
-        Event x3 = new Event("Event 3");
-        eventc.save(x1);
-        eventc.save(x2);
-        eventc.save(x3);
-        assertEquals(3,repo.count());
-        assertTrue(repo.calledMethods.contains("count"));
+        assertEquals(3, repo.count());
+        assertEquals("count", repo.calledMethods.getLast());
+
         var actual = eventc.getAll();
-        assertTrue(repo.calledMethods.contains("findAll"));
-        assertEquals(x1,actual.getFirst());
+        assertEquals(OK, actual.getStatusCode());
+        assertEquals("findAll", repo.calledMethods.getLast());
+        assertEquals(List.of(x1, x2, x3), actual.getBody());
     }
 
 
     @Test
-    void save() {
+    void save1() {
         var actual = eventc.save(new Event(null));
         assertEquals(BAD_REQUEST, actual.getStatusCode());
-        assertFalse(repo.calledMethods.contains("save"));
-        eventc.save(new Event("Event 1"));
+    }
+
+    @Test
+    void save2() {
+        var actual = eventc.save(new Event(""));
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    void save3() {
+        Event x = new Event("k");
+        x.setId(null);
+        var actual = eventc.save(x);
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    void save4() {
+        Event x = new Event("k");
+        x.setId("");
+        var actual = eventc.save(x);
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    void save5() {
+        Event x = new Event("k");
+        long c = repo.count();
+        var actual = eventc.save(x);
+        assertEquals(OK, actual.getStatusCode());
         assertTrue(repo.calledMethods.contains("save"));
+        assertEquals(c+1, repo.count());
+    }
+
+    @Test
+    void save6() {
+        Event x = new Event("k");
+        long c = repo.count();
+        x.setId(x1.getId());
+        var actual = eventc.save(x);
+        assertEquals(OK, actual.getStatusCode());
+        assertTrue(repo.calledMethods.contains("save"));
+        assertEquals(c, repo.count());
     }
 
     @Test
     void getById() {
-        Event x = new Event("Event 1");
-        Event event = eventc.save(x).getBody();
         var bad_req = eventc.getById("");
-        assertEquals(BAD_REQUEST, bad_req.getStatusCode());
-        Event find = eventc.getById(event.getId()).getBody();
+        assertEquals(NOT_FOUND, bad_req.getStatusCode());
+
+        var good_req = eventc.getById(x1.getId());
         assertTrue(repo.calledMethods.contains("findById"));
-        assertEquals(event, find);
+        assertEquals(x1, good_req.getBody());
+        assertEquals(OK, good_req.getStatusCode());
     }
 
-    @Test
-    void getByInviteCode() {
-        Event x = new Event("Test");
-        Event event = eventc.save(x).getBody();
-        String inviteCode = event.getId();
-        assertEquals(event, eventc.getById(inviteCode).getBody());
-        assertEquals(BAD_REQUEST, eventc.getById(null).getStatusCode());
-    }
 
     @Test
     void deleteById() {
-        Event x1 = new Event("Event 1");
-        Event x2 = new Event("Event 2");
-        Event x3 = new Event("Event 3");
-        eventc.save(x1);
-        eventc.save(x2);
-        eventc.save(x3);
-        var actual = eventc.deleteById(eventc.getAll().getFirst().getId());
         var bad_req = eventc.deleteById("c");
+        assertEquals(NOT_FOUND, bad_req.getStatusCode());
+
+        var c = repo.count();
+        var actual = eventc.deleteById(x1.getId());
         assertTrue(repo.calledMethods.contains("deleteById"));
         assertEquals(x1, actual.getBody());
+        assertEquals(OK, actual.getStatusCode());
         assertFalse(repo.events.contains(x1));
         assertTrue(repo.events.contains(x3));
-        assertEquals(BAD_REQUEST, bad_req.getStatusCode());
+        assertEquals(c-1, repo.count());
     }
 
     @Test
-    void updateTitle() {
-        Event x1 = new Event("Event 1");
-        eventc.save(x1);
-        eventc.updateTitle(eventc.getAll().getFirst().getId(),"New Event");
-        var bad_req = eventc.updateTitle("", "Test");
-        assertTrue(repo.calledMethods.contains("save"));
-        assertEquals("New Event",eventc.getAll().getFirst().getTitle());
+    void updateTitle1() {
+        var bad_req = eventc.updateTitle(x1.getId(), "");
         assertEquals(BAD_REQUEST, bad_req.getStatusCode());
+
+        var bad_req2 = eventc.updateTitle(x1.getId(), null);
+        assertEquals(BAD_REQUEST, bad_req2.getStatusCode());
     }
+
+    @Test
+    void updateTitle2() {
+        var bad_req = eventc.updateTitle(null, "kebabai");
+        assertEquals(NOT_FOUND, bad_req.getStatusCode());
+    }
+
+    @Test
+    void updateTitle3() {
+        var actual = eventc.updateTitle(x1.getId(), "randomEvent");
+        assertEquals(OK, actual.getStatusCode());
+        assertEquals(x1, actual.getBody());
+    }
+
+
 }
